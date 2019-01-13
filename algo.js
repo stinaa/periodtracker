7eba630c3ec9ff21affd3ea4c7ba4784281c2bfd
Stina Andersson <stina.andersson@naturalcycles.com>
2019-01-13 16:48:27 +0100

    create period tracking app ui

---
+++ algo.js
@@ -0,0 +1,3 @@
+predictNextPeriodStartDate = (periods) => {
+  return '🤷'
+}
--- app.js
+++ app.js
@@ -1,22 +1,67 @@
+let calendar
+
 const init = () => {
   const dates = JSON.parse(localStorage.getItem('period_dates'))
 
-  flatpickr('#dates', {
+  calendar = flatpickr('#dates', {
     mode: 'multiple',
     maxDate: new Date(),
     defaultDate: dates,
-    onChange: datesChanged
+    onChange: datesChanged,
+    inline: true
   })
+
+  if (dates) {
+    const arr = dates.map((d) => {
+      return new Date(d)
+    })
+
+    datesChanged(arr)
+  } 
 }
 
 const datesChanged = (dates) => {
+  dates.sort((a, b) => {
+    return a - b
+  })
+  
+  // Save dates in local storage
   localStorage.setItem('period_dates', JSON.stringify(dates))
 
+  const periods = getPeriodsFromDates(dates)
+  const nextPeriod = predictNextPeriodStartDate(periods)
+
+  document.getElementById('prediction').innerHTML = nextPeriod
+}
+
+const getPeriodsFromDates = (dates) => {
+  const periods = []
+  let previousDate = undefined
+
   dates.forEach(date => {
-    console.log(date.toDateString())
+    if (!previousDate) {
+      previousDate = date
+    }
+
+    if (daysBetween(previousDate, date) !== 1) {
+      periods.push([])
+    }
+
+    periods[periods.length-1].push(date)
+    
+    previousDate = date
   })
 
+  return periods
+}
+
+const daysBetween = (firstDate, secondDate) => {
+  const oneDay = 24 * 60 * 60 * 1000 // hours * minutes * seconds * milliseconds
+  return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / oneDay))
+}
+
+const clearDates = () => {
+  calendar.clear()
 }
 
 init()
-
