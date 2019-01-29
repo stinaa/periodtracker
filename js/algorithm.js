29dad44bf55e10082edd6b53e7c7c928b76d0b4a
Stina Andersson <stina.andersson@naturalcycles.com>
2019-01-29 13:29:18 +0100

    bugfix in the algorithm

--- js/algorithm.js
+++ js/algorithm.js
@@ -8,12 +8,15 @@ class PeriodAlgo {
 
     // Step 1. Calculate the average cycle length
     // Tip! Use the numbers in the array data.cycleLengths to get the average value
+
     
     // Step 2. Use the average cycle length to predict when the next cycle will start 
     // Tip! You might want to use data.currentCycleStart and the value you got from step 1 to get the new date
 
+
     // Step 3. Return the predicted period date
 
+    
   }
 
 
@@ -43,7 +46,7 @@ class PeriodAlgo {
     if (!date || !days) return
 
     // Modify the date by adding X amount of days
-    const modifiedDate = new Date().setDate(date.getDate() + days)
+    const modifiedDate = new Date(date).setDate(date.getDate() + days)
 
     // Return the modified date
     return new Date(modifiedDate)
